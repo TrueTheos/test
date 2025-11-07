59a4ca2bcac3d377a508c1e4630f7d927801234c
Theos <loiuy538@gmail.com>
2025-11-07 23:33:19 +0100

    x

---
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Avalonia.Controls;
+using LibVLCSharp.Shared;
+
+namespace AvaloniaApplication1.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    private readonly LibVLC _libVlc = new LibVLC();
+        
+    public MediaPlayer MediaPlayer { get; }
+        
+    public MainWindowViewModel()
+    {
+        MediaPlayer = new MediaPlayer(_libVlc);
+        Play();
+    }
+
+    public void Play()
+    {
+        if (Design.IsDesignMode)
+        {
+            return;
+        }
+            
+        using var media = new Media(_libVlc, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
+        MediaPlayer.Play(media);
+    }
+        
+    public void ToggleFullscreen()
+    {
+        MediaPlayer.ToggleFullscreen();
+    }
+
+}
